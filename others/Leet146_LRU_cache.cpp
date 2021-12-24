e8eab4cf769b5ab4100efdc171a2e23521ae3c5c
Lan <jianglanniovo@gmail.com>
2021-12-23 19:41:55 -0800

    Create Leet146_LRU_cache.cpp

---
+++ others/Leet146_LRU_cache.cpp
@@ -0,0 +1,102 @@
+class LRUCache {
+public:
+    unordered_map<int, int> m;
+    map<int, void*> addr;  // *addr[key] == address of this node
+    int capacity;
+    int size = 0;
+
+    class ListNode {
+    public:
+        int val;
+        ListNode* prev;
+        ListNode* next;
+        ListNode() {}
+        ListNode(int val) {
+            this->val = val;
+        }
+        ListNode(int val, ListNode* prev, ListNode* next) {
+            this->val = val;
+            this->prev = prev;
+            this->next = next;
+        }
+    };
+
+    ListNode* head;
+    ListNode* tail;
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+        head = new ListNode(-1);
+        tail = new ListNode(-1, head, nullptr);
+        head->next = tail;
+    }
+
+    void list_push(void* node_address) {
+        ListNode* node = static_cast<ListNode*> (node_address);
+        ListNode* original_tail = tail->prev;
+        original_tail->next = node;
+        node->prev = original_tail;
+        node->next = tail;
+        tail->prev = node;
+    }
+
+    void list_remove(void* node_address) {
+        ListNode* node = static_cast<ListNode*> (node_address);
+        node->next->prev = node->prev;
+        node->prev->next = node->next;
+        node->prev = nullptr;
+        node->next = nullptr;
+    }
+
+    int get(int key) {
+        if (addr.find(key) != addr.end() && addr.at(key) != nullptr) {
+            list_remove(addr[key]);
+            list_push(addr[key]);
+            return m.at(key);
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        m[key] = value;
+        if (addr.find(key) == addr.end()) {
+            addr.emplace(key, nullptr);
+        }
+        // Update an element in the cache
+        if (addr[key] != nullptr) {
+            list_remove(addr[key]);
+            list_push(addr[key]);
+            return;
+        }
+        // Condition: addr[key] = nullptr
+        // Throwing a new element into the cache
+        addr[key] = new ListNode(key);
+        list_push(addr[key]);
+        if (size < capacity) {
+            ++size;  // Simply increment the size
+        } else {
+            // Pop the oldest element
+            ListNode* victim = head->next;
+            addr[victim->val] = nullptr;
+            list_remove(victim);
+            delete victim;
+        }
+    }
+
+    ~LRUCache() {
+        ListNode* n = head;
+        while (true) {
+            n = n->next;
+            delete n->prev;
+            if (n == tail)
+                break;
+        }
+        delete tail;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
